02ff43b218def9d50237ef29eedccab3682a53e2
Vladimir <32330413+GlendenCrunch@users.noreply.github.com>
2021-11-12 08:43:34 +0300

    Update lct3_e502.py

--- lct3_e502.py
+++ lct3_e502.py
@@ -39,22 +39,13 @@ class t_x502_info(Structure):
 # --------------------------------------------------------------------------------
 pp = ctypes.pointer(Read_x502())
 pp2 = ctypes.pointer(t_x502_info())
-#pp3 = ctypes.pointer(t_x502_cbr())
-#pp4 = ctypes.pointer(t_x502_cbr_coef())
 
 lib = cdll.LoadLibrary('lib\\e502api.dll')
 lib2 = cdll.LoadLibrary('lib\\x502api.dll')
 
-#UsbGet = lib.E502_UsbGetSerialList(pp.contents.arr, 2, 0, pp.contents.devs)
-#UsbGet = lib.E502_UsbGetDevRecordsList(pp.contents.arr, 2, 0, pp.contents.devs)
-#print ('UsbGet:', UsbGet)
-#print ('devs:', pp.contents.devs)
-#print ('arr:', pp.contents.arr[0])  # нулевой индекс, т.к. одна плата
-
 Create = lib2.X502_Create()
 print ('Create:', Create)
 
-#Open = lib.X502_OpenByDevRecord(Create, 0)
 Open = lib.E502_OpenUsb(Create, 0)  # 0 - конект к первой подключенной плате
 print ('Open:', Open)
 
@@ -67,9 +58,6 @@ print ('fpga_ver:', pp2.contents.fpga_ver)
 print ('plda_ver:', pp2.contents.plda_ver)
 print ('board_rev:', pp2.contents.board_rev)
 print ('mcu_firmware_ver:', pp2.contents.mcu_firmware_ver)
-#print ('factory_mac:', pp2.contents.factory_mac)
-#print ('rezerv:', pp2.contents.rezerv)
-#print ('cbr:', pp2.contents.cbr)
 
 for i in range(6):
     print ('-->ADC cbr_offs {}: {}'.format(i, pp2.contents.cbr.adc[i].offs))
@@ -90,7 +78,7 @@ volt_dac = array.array('f', ())     # AC
 Um = 2000                           # rms mV
 f = 10000                           # Hz
 size = 1000000
-dac_numb =  0x10
+dac_numb =  0x10                    # 0x10-ЦАП1; 0x20-ЦАП2
 for x in range(size):
     y_sin_analog = (Um * (math.sin(x * 2 * math.pi * f / size))) * math.sqrt(2) * (30000 / 5000)
     volt_dac.append(y_sin_analog)
@@ -118,5 +106,4 @@ def close():
 
 if __name__ == '__main__':
     dac_synchr()
-    #dac_cycle()
     close()
